6368ececf71afea82abc181d27d781c0b54c385f
mirse <mirsent@outlook.com>
2019-01-08 17:02:08 +0800

    广告收藏管理

--- Application/Admin/Controller/AdvertiserController.class.php
+++ Application/Admin/Controller/AdvertiserController.class.php
@@ -78,4 +78,62 @@ class AdvertiserController extends AdminBaseController{
         ajax_return(1);
     }
 
+    /**
+     * 获取广告收藏信息
+     */
+    public function get_collection_info()
+    {
+        $ms = D('Collect');
+        $cond = [
+            'c.status' => C('STATUS_Y')
+        ];
+
+        $recordsTotal = $ms->alias('c')->where($cond)->count();
+
+        // 搜索
+        $search = I('search');
+        if (strlen($search)>0) {
+            $cond['ad_title|nickname'] = array('like', '%'.$search.'%');
+        }
+        $cond['ad_title'] = I('ad_title');
+        $cond['nickname'] = I('nickname');
+
+        $searchDate = I('collect_time');
+        if (strlen($searchDate)) {
+            $cond['collect_time'] = array('between', [$searchDate.' 00:00:00', $searchDate.' 23:59:59']);
+        }
+
+        $recordsFiltered = $ms->getCollectNumber($cond);
+
+        // 排序
+        $orderObj = I('order')[0];
+        $orderColumn = $orderObj['column'];
+        $orderDir = $orderObj['dir'];
+        if(isset(I('order')[0])){
+            $i = intval($orderColumn);
+            switch($i){
+                case 1: $ms->order('ad_title '.$orderDir); break;
+                case 2: $ms->order('nickname '.$orderDir); break;
+                case 3: $ms->order('collect_time '.$orderDir); break;
+                default: break;
+            }
+        } else {
+            $ms->order('collect_time desc');
+        }
+
+        // 分页
+        $start = I('start');
+        $limit = I('limit');
+        $page = I('page');
+
+        $infos = $ms->page($page, $limit)->getCollectData($cond);
+
+        echo json_encode(array(
+            "draw" => intval(I('draw')),
+            "recordsTotal" => intval($recordsTotal),
+            "recordsFiltered" => intval($recordsFiltered),
+            "data" => $infos
+        ), JSON_UNESCAPED_UNICODE);
+    }
+
 }
---
+++ Application/Common/Model/CollectModel.class.php
@@ -0,0 +1,32 @@
+<?php
+namespace Common\Model;
+use Common\Model\BaseModel;
+class CollectModel extends BaseModel{
+    protected $_auto=array(
+        array('status','get_default_status',1,'callback'),
+        array('collect_time','get_time',1,'callback'),
+    );
+
+    public function getCollectNumber($cond=[])
+    {
+        $data = $this
+            ->alias('c')
+            ->join('__ADVERTISE__ ad ON ad.id = c.ad_id')
+            ->join('__ADVERTISER__ ader ON ader.id = c.ader_id')
+            ->where(array_filter($cond))
+            ->count();
+        return $data;
+    }
+
+    public function getCollectData($data=[])
+    {
+        $data = $this
+            ->alias('c')
+            ->join('__ADVERTISE__ ad ON ad.id = c.ad_id')
+            ->join('__ADVERTISER__ ader ON ader.id = c.ader_id')
+            ->field('c.*,ad.ad_title,ader.nickname')
+            ->where(array_filter($cond))
+            ->select();
+        return $data;
+    }
+}
